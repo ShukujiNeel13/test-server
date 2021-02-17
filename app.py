c13e4e76874bc127095c93911ddf1b4048262a80
ShukujiNeel13 <neelshukuji@gmail.com>
2021-02-17 17:44:41 +0800

    Add whitespaces where required

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
+import time
 import json
 from pprint import pprint
 from flask import Flask, request, make_response, jsonify
 
 app = Flask(__name__)
 
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    """Echoes back the request body as response body if one is given."""
+
     request_status = f'{request.method} request received on index.'
     print(request_status)
     response_data = {'status': request_status}
@@ -19,6 +23,14 @@ def index():
         pprint(request_body_dict)
 
     return make_response(jsonify(response_data), 200)
+
+
+@app.route('/index_with_delay', methods=['GET', 'POST'])
+def index_with_delay():
+
+    print('Request received on /index_with_delay')
+    time.sleep(9)
+    return make_response(jsonify({'status': 'success'}), 200)
 
 
 if __name__ == '__main__':
