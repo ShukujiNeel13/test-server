20ba3d39b1d71688d38ece456368de404a56efb5
ShukujiNeel13 <neelshukuji@gmail.com>
2022-03-10 14:17:19 +0800

    Add module for definitions

---
+++ definitions.py
@@ -0,0 +1,9 @@
+# region Data Model: Checker functions
+CHECKER_ATTR_STATUS = 'status'
+CHECKER_ATTR_TEXT = 'text'
+
+CHECKER_MODEL_DATA_RETURN = {
+    CHECKER_ATTR_TEXT: 'default',
+    CHECKER_ATTR_STATUS: False
+}
+# endregion
