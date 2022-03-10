3cbf8ae6fabda44ac406bb34fe31af01f77f79d7
ShukujiNeel13 <neelshukuji@gmail.com>
2022-03-10 18:15:40 +0800

    Make progress to verify process (pending testing)

--- jwt.py
+++ jwt.py
@@ -1,7 +1,10 @@
 import base64
 import json
 import os.path
+from time import time
 from pprint import pformat
+
+import utils
 
 SAMPLE_AUTH_RESPONSE = {
     'code': None,
@@ -27,7 +30,8 @@ def _check_jwt_token_structure_valid(jwt_token: str) -> dict:
     response_data = {
         'status': False,
         'message': 'Structure invalid',
-        'token_sections_by_name': None
+        'token_sections_by_name': None,
+        'jwtVerifyStepNumber': 1
     }
 
     _separating_char = '.'
@@ -71,7 +75,8 @@ def _check_jwt_signature_valid(b64_encoded_signature: str) -> dict:
 
     response_data = {
         'status': False,
-        'message': 'Signature invalid'
+        'message': 'Signature invalid',
+        'jwtVerifyStepNumber': 2
     }
 
     _key = '?'
@@ -89,20 +94,21 @@ def _check_jwt_signature_valid(b64_encoded_signature: str) -> dict:
     return response_data
 
 
-# TODO: Get the header from the unhased Signature?
-def _check_header_kid_matches_key(kid: str, kid_alg: str = 'RS256', json_filepath_jwt_keys: str = None) -> dict:
+# TODO: Get the header from the unhashed Signature?
+def _check_header_kid_matches_any_public_key(local_kid: str, kid_alg: str = 'RS256', json_filepath_jwt_keys: str = None) -> dict:
     """
     Compare the local key ID (kid) to the public kid.
 
-    :param header_kid:
-    :param algorithm:
-    :param match_how:
+    :param kid:
+    :param kid_alg:
+    :param json_filepath_jwt_keys:
     :return:
     """
 
     response_data = {
         'status': False,
-        'message': 'default'
+        'message': 'default',
+        'jwtVerifyStepNumber': 3
     }
 
     # region Get keys data from the public jwt keys json file
@@ -133,32 +139,78 @@ def _check_header_kid_matches_key(kid: str, kid_alg: str = 'RS256', json_filepat
 
     no_of_keys_data = len(list_of_keys_data)
 
+    # region If no of keys in public jwt keys json file is 1 - do matching
     if no_of_keys_data == 1:
         key_data = list_of_keys_data[0]
+        _get_kid_resp = utils.extract_attr_from_data(key_data, 'key data', 'kid', str)
+        public_kid = _get_kid_resp['value']
+        get_kid_text = _get_kid_resp['text']
 
-    else:
-        # Group by "alg"
+        if public_kid is None:
+            response_data['message'] = f'Failed to get kid from {_file_data_name} ({get_kid_text})'
+            return response_data
 
-    _group_by_alg_resp = _group_jwt_public_keys_by_attribute(
+        if local_kid == public_kid:
+            response_data['status'] = True
+            response_data['message'] = 'Local kid matched public kid'
+        else:
+            response_data['message'] = 'Local kid did not match public kid'
+
+        return response_data
+    # endregion
+
+    # region Get public keys data matching "alg" of given key - return if none match
+    _get_jwt_public_keys_matching_alg_resp = _get_jwt_public_keys_matching_alg(
         list_of_keys_data,
-        attr_name='alg',
-        attr_value=kid_alg
+        alg_value=kid_alg
     )
 
-    _key = '?'
-    _invalid_status_text = f'Key ID in JWT header section must match the public key id'
+    keys_data_matching_alg = _get_jwt_public_keys_matching_alg_resp['data']
+    get_jwt_public_keys_matching_alg_message = _get_jwt_public_keys_matching_alg_resp['message']
 
-    invalid = True
-    if invalid:
-        response_data['message'] += f' ({_invalid_status_text})'
+    if keys_data_matching_alg is None:
+        response_data['message'] = get_jwt_public_keys_matching_alg_message
+        return response_data
+    # endregion
+
+    if len(keys_data_matching_alg) == 1:
+        key_data = keys_data_matching_alg[0]
+
+        _get_kid_resp = utils.extract_attr_from_data(key_data, 'public key data', 'kid', str)
+        public_kid = _get_kid_resp['value']
+        get_kid_text = _get_kid_resp['text']
+
+        if public_kid is None:
+            response_data['message'] = f'Failed to get kid from {_file_data_name} ({get_kid_text})'
+            return response_data
+
+        if local_kid == public_kid:
+            response_data['status'] = True
+            response_data['message'] = 'Local kid matched a public kid'
+        else:
+            response_data['message'] = 'Local kid did not match public kid'
+
+        return response_data
+    else:
+        for key_data in keys_data_matching_alg:
+
+            _get_kid_resp = utils.extract_attr_from_data(key_data, 'key data', 'kid', str)
+            public_kid = _get_kid_resp['value']
+            get_kid_text = _get_kid_resp['text']
+
+            if public_kid is None:
+                print(f'Failed to get kid from {_file_data_name} ({get_kid_text})')
+                continue
+
+            if local_kid == public_kid:
+                response_data['status'] = True
+                response_data['message'] = 'Local kid matched a public kid'
+                return response_data
+        response_data['message'] = f'local kid did not match any of the public kid having alg = {kid_alg}'
         return response_data
 
-    response_data['status'] = True
-    response_data['message'] = 'JWT signature section is valid'
-    return response_data
 
-
-def _group_jwt_public_keys_by_attribute(keys_data: list, attr_name: str, attr_value: str) -> dict:
+def _get_jwt_public_keys_matching_alg(keys_data: list, alg_value: str = 'RS256') -> dict:
 
     response_data = {
         'data': None,
@@ -169,11 +221,32 @@ def _group_jwt_public_keys_by_attribute(keys_data: list, attr_name: str, attr_va
 
     if no_of_keys_data == 1:
         key_data = keys_data[0]
+        _get_alg_resp = utils.extract_attr_from_data(key_data, 'public key data', 'alg', str)
+
+        alg = _get_alg_resp['value']
+        get_alg_text = _get_alg_resp['text']
+
+        if alg is None:
+            response_data['message'] = f'This public key data is invalid ({get_alg_text})'
+            return response_data
+
+        if alg == alg_value:
+            response_data['data'] = key_data
+            response_data['message'] = f'Public key data found with alg = {alg_value}'
+        else:
+            response_data['message'] = f'No keys found with alg = {alg}'
+
+        return response_data
     else:
-        for key_data in keys_data:
+        keys_data_matching_alg = [key_data for key_data in keys_data if key_data['alg'] == alg_value]
 
+        if not keys_data_matching_alg:
+            response_data['message'] = f'None of the keys data have alg = {alg_value}'
+            return response_data
 
-    return response_data
+        response_data['data'] = keys_data_matching_alg
+        response_data['message'] = f'Obtained keys data with alg = {alg_value}'
+        return response_data
 
 
 def _get_data_from_jwt_keys_json_file(json_filepath: str = None) -> dict:
@@ -201,6 +274,10 @@ def _get_data_from_jwt_keys_json_file(json_filepath: str = None) -> dict:
 
         response_data['data'] = keys_data
 
+
+def _check_token_not_expired():
+
+
 # endregion
 
 
@@ -217,37 +294,191 @@ def check_jwt_valid(jwt_token: str = None) -> dict:
     _check_structure_resp = _check_jwt_token_structure_valid(jwt_token)
     structure_valid = _check_structure_resp['status']
     check_structure_message = _check_structure_resp['message']
+    # process_step_number = _check_structure_resp['jwtVerifyStepNumber']
 
     if structure_valid is False:
-        response_data['message'] = check_structure_message
+        response_data['message'] = f'Step 1 failed. {check_structure_message}'
         return response_data
     # endregion
 
     token_sections_by_name = _check_structure_resp['token_sections_by_name']
 
-    token_signature_b64_string = token_sections_by_name['signature']
+    # token_signature_b64_string = token_sections_by_name['signature']
 
     # region Check signature section valid
-    _check_signature_resp = _check_jwt_signature_valid(token_signature_b64_string)
-    structure_valid = _check_structure_resp['status']
-    check_structure_message = _check_structure_resp['message']
+    # _check_signature_resp = _check_jwt_signature_valid(token_signature_b64_string)
+    # signature_valid = _check_signature_resp['status']
+    # check_signature_message = _check_signature_resp['message']
+    #
+    # if structure_valid is False:
+    #     response_data['message'] = f'Step 2 failed. {check_signature_message}'
+    #     return response_data
+    # endregion
 
-    if structure_valid is False:
-        response_data['message'] = check_structure_message
+    token_header = token_sections_by_name['header']
+
+    # region Decode token header
+    _decoded_token_header_bytes = base64.b64decode(token_header)
+    _decoded_token_header_data_json_string = _decoded_token_header_bytes.decode('utf-8')
+    decoded_token_header_data = json.loads(_decoded_token_header_data_json_string)
+    # endregion
+
+    # region Get kid from decoded token header data
+    _get_kid_resp = utils.extract_attr_from_data(
+        decoded_token_header_data,
+        'decoded jwt header',
+        'kid',
+        str
+    )
+
+    local_kid = _get_kid_resp['value']
+    get_kid_text = _get_kid_resp['text']
+
+    if local_kid is None:
+        response_data['message'] = get_kid_text
         return response_data
     # endregion
 
-    token_header_b64_string = token_sections_by_name['header']
+    # region Get alg from decoded token header data
+    _get_alg_resp = utils.extract_attr_from_data(
+        decoded_token_header_data,
+        'decoded jwt header',
+        'alg',
+        str
+    )
+
+    local_key_alg = _get_alg_resp['value']
+    get_alg_text = _get_alg_resp['text']
+
+    if local_key_alg is None:
+        response_data['message'] = get_alg_text
+        return response_data
+
+    # endregion
 
     # region Check header key id matches public key id
-    _check_signature_resp = _check_header_kid_matches_key(token_header_b64_string)
-    structure_valid = _check_structure_resp['status']
-    check_structure_message = _check_structure_resp['message']
 
-    if structure_valid is False:
-        response_data['message'] = check_structure_message
+    _check_header_key_id_valid_resp = _check_header_kid_matches_any_public_key(local_kid, local_key_alg)
+    key_valid = _check_header_key_id_valid_resp['status']
+    check_key_valid_message = _check_header_key_id_valid_resp['message']
+
+    if key_valid is False:
+        response_data['message'] = f'Step 3 failed. {check_key_valid_message}'
         return response_data
+
     # endregion
+
+    token_payload = token_sections_by_name['payload']
+
+    # region Decode token payload
+    _decoded_token_payload_bytes = base64.b64decode(token_payload)
+    _decoded_token_payload_data_json_string = _decoded_token_payload_bytes.decode('utf-8')
+    decoded_token_payload_data = json.loads(_decoded_token_payload_data_json_string)
+    # endregion
+
+    # region Get exp from decoded token payload data
+    _get_exp_resp = utils.extract_attr_from_data(
+        decoded_token_payload_data,
+        'decoded jwt payload',
+        'exp',
+        int
+    )
+
+    token_exp = _get_exp_resp['value']
+    get_exp_text = _get_exp_resp['text']
+
+    if token_exp is None:
+        response_data['message'] = get_exp_text
+        return response_data
+
+    # endregion
+
+    curr_unix_time = int(time())
+
+    if token_exp < curr_unix_time:
+        response_data['message'] = 'Given token has already expired'
+        return response_data
+
+    # region Get token_use from decoded token payload data
+    _get_token_use_resp = utils.extract_attr_from_data(
+        decoded_token_payload_data,
+        'decoded jwt payload',
+        'token_use',
+        str
+    )
+
+    token_use = _get_token_use_resp['value']
+    get_token_use_text = _get_token_use_resp['text']
+
+    if token_use is None:
+        response_data['message'] = get_token_use_text
+        return response_data
+
+    # endregion
+
+    if token_use != 'access':
+        response_data['message'] = f'token_use in decoded token payload is: {token_use} (Required: "access")'
+        return response_data
+
+    # region Get iat from decoded token payload data
+    _get_iat_resp = utils.extract_attr_from_data(
+        decoded_token_payload_data,
+        'decoded jwt payload',
+        'iat',
+        int
+    )
+
+    token_iat = _get_iat_resp['value']
+    get_iat_text = _get_iat_resp['text']
+
+    if token_iat is None:
+        response_data['message'] = get_iat_text
+        return response_data
+
+    # endregion
+
+    if token_iat > curr_unix_time:
+        response_data['message'] = f'UNEXPECTED: iat in token payload is greater than current unix time'
+        return response_data
+
+    # region Get client_id from decoded token payload data
+    _get_client_id_resp = utils.extract_attr_from_data(
+        decoded_token_payload_data,
+        'decoded jwt payload',
+        'client_id',
+        str
+    )
+
+    token_client_id = _get_client_id_resp['value']
+    get_client_id_text = _get_client_id_resp['text']
+
+    if token_client_id is None:
+        response_data['message'] = get_client_id_text
+        return response_data
+
+    # endregion
+
+    # TODO: Require client id to verify with token client id...
+
+    # region Get scope from decoded token payload data
+    _get_scope_resp = utils.extract_attr_from_data(
+        decoded_token_payload_data,
+        'decoded jwt payload',
+        'scope',
+        str
+    )
+
+    token_scope = _get_scope_resp['value']
+    get_scope_text = _get_scope_resp['text']
+
+    if token_scope is None:
+        response_data['message'] = get_scope_text
+        return response_data
+
+    # endregion
+
+    # TODO: Require service op name to verify with token scope
+
 
 
 if __name__ == '__main__':
